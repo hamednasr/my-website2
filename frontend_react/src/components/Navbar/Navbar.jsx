616671d9a9ec55e7fbab89df8dfad80e0ddf6458
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2022-08-08 10:22:51 -0400

    navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" width={60} />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div />
+            <a href={`#{item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
